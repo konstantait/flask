d9049e49a8a1a9196f646c9800ee7438cc7ba4e5
John Doe <konstanta.it@gmail.com>
2023-03-13 15:55:16 +0200

    initial release

---
+++ apps/config.py
@@ -0,0 +1,27 @@
+import os
+from decouple import config
+
+
+class Config(object):
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_007')
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ...
+
+
+class DebugConfig(Config):
+    DEBUG = True
+
+
+config_dict = {
+    'Production': ProductionConfig,
+    'Debug': DebugConfig
+}
---
+++ run.py
@@ -0,0 +1,27 @@
+from sys import exit
+from flask_migrate import Migrate
+from decouple import config
+
+from apps import create_app, db
+from apps.config import config_dict
+
+
+DEBUG = config('DEBUG', default=True, cast=bool)
+
+get_config_mode = 'Debug' if DEBUG else 'Production'
+
+try:
+    app_config = config_dict[get_config_mode.capitalize()]
+except KeyError:
+    exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
+
+app = create_app(app_config)
+Migrate(app, db)
+
+if DEBUG:
+    app.logger.info('DEBUG       = ' + str(DEBUG))
+    app.logger.info('Environment = ' + get_config_mode)
+    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
+
+if __name__ == "__main__":
+    app.run()
